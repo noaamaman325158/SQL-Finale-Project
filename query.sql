fe52628fd67d016e48079b7755a43aee16652131
Noaa Maman <noaamaman325158@gmail.com>
2023-02-25 23:30:11 +0200

    addBookInitialProject

--- query.sql
+++ query.sql
@@ -2,7 +2,6 @@ SELECT
 	COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
     COUNT(DISTINCT orders.order_id) AS orders,
     COUNT(DISTINCT orders.order_id) / COUNT(DISTINCT website_sessions.website_session_id) AS session_to_order_cvr
-	
 FROM website_sessions
 	LEFT JOIN orders
 		ON orders.website_session_id = website_sessions.website_session_id
